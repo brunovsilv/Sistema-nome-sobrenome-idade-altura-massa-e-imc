671c43c5f1b88e3c30deebe55f228896c98a7728
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-06-13 02:01:19 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner SC = new
+     Scanner(System.in);
+    
+    System.out.println("Digite seu nome:");
+    String nome = SC.next();
+
+    System.out.println("Digite seu sobre nome:");
+    String sobrenome = SC.next();
+
+    System.out.println("idade:");
+    String idade = SC.next();
+
+    System.out.println("altura:");
+    String altura = SC.next();
+
+    System.out.println("massa:");
+    String massa = SC.next();
+
+    System.out.println("calcular imc 90 ÷ 1,80:");
+    String imc = SC.next();
+    
+    
+  }
+}
